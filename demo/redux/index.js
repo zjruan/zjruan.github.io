ccf2f1f96223f5ea018f618beab431bd08917793
阮照进 <rzhj@foxmail.com>
2016-05-27 19:10:14 +0800

    redux

---
+++ demo/redux/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable semi */ 
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import todoApp from './reducers';
+/* eslint-enable semi */
+
+let store = createStore(todoApp);
+let rootElement = document.getElementById('myApp');
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,   
+    rootElement
+);
